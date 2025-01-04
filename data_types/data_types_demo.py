5b9433442685d502c780a187591d0e88d6108b46
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2025-01-04 21:11:51 +0600

    Data Types

---
+++ data_types/data_types_demo.py
@@ -0,0 +1,18 @@
+age = 20
+my_age = 30
+tax_rate = 10.5
+my_name = 'Alice'
+city_name = ['dhaka', 'NY', 'London', 'paris']
+_fruits = ('apple', 'banana', 'orange')
+age2 = 40
+# multiple variable assignments
+x, y, z = "Orange", "Banana", "Cherry"
+a = b = c = "Orange"
+
+"""
+this is multiple comments example
+hello world
+"""
+
+print(age)
+
